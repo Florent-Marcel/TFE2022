aa03fd3dcc9cd9568645542ede6631d0304b07f7
Florent-Marcel <flo.marcel@hotmail.com>
2022-07-23 21:33:11 +0200

    migration without relation for simple table

---
+++ database/migrations/2022_07_23_192806_create_personalities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personalities', function (Blueprint $table) {
+            $table->id();
+            $table->char("firstname", 128);
+            $table->char("lastname", 128);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personalities');
+    }
+};
