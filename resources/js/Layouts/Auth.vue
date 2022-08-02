751ecb601d79b34e486f33286f796f14c246ce64
Florent Marcel <florent.marcel@keysource.be>
2022-08-02 18:14:06 +0200

    First layout and page movies

---
+++ resources/js/Layouts/Auth.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from 'vue';
+import BreezeApplicationLogo from '@/Components/ApplicationLogo.vue';
+import BreezeDropdown from '@/Components/Dropdown.vue';
+import BreezeDropdownLink from '@/Components/DropdownLink.vue';
+import BreezeNavLink from '@/Components/NavLink.vue';
+import BreezeResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import { Link } from '@inertiajs/inertia-vue3';
+import { defineComponent } from 'vue'
+import Header from '@/Components/HeaderAuth.vue'
+import Menu from '@/Components/Menu.vue'
+
+const showingNavigationDropdown = ref(false);
+</script>
+
+<template>
+    <Header></Header>
+    <Menu></Menu>
+    <main class="main">
+        <slot />
+    </main>
+</template>
+
+
+<style>
+@font-face {
+    font-family: "Nunito";
+    src: url('/fonts/NunitoSans-Regular.ttf');
+}
+
+@font-face {
+    font-family: "Nunito-black";
+    src: url("/fonts/NunitoSans-Black.ttf");
+}
+
+html {
+    background: #F4F3F7;
+}
+
+html * {
+    font-family: "Nunito";
+    font-size: 14px;
+}
+
+body {
+    margin: 0px 0px 8px 0px;
+}
+
+a {
+    text-decoration: none;
+    color: inherit;
+}
+
+h3{
+    font-family: 'Nunito-black';
+    font-size: 19px;
+}
+</style>
+
--- resources/js/Pages/Movies.vue
+++ resources/js/Pages/Movies.vue
@@ -1,5 +1,5 @@
 <script setup>
-import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import BreezeAuthenticatedLayout from '@/Layouts/Auth.vue';
 import { defineComponent } from 'vue'
 import { Head } from '@inertiajs/inertia-vue3';
 </script>
@@ -14,17 +14,26 @@ import { Head } from '@inertiajs/inertia-vue3';
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        <div v-for="movie in movies" :key="movie.id">
-                            {{movie.title}}
-                        </div>
-                    </div>
+        <div class="title">
+            <h3>Liste des films à l'affiche</h3>
+        </div>
+        <div class="movie-wrapper">
+            <div class="movie-rectangle" v-for="movie in movies" :key="movie.id">
+                <div class="movie-title">
+                    <span>{{movie.title}}</span>
+                </div>
+                <div class="movie-poster">
+                    <img>
+                </div>
+                <div class="movie-dates" v-if="findExtremitySceance(movie.showings, true) != findExtremitySceance(movie.showings, false)">
+                    Du {{dateToString(findExtremitySceance(movie.showings, true))}} au {{dateToString(findExtremitySceance(movie.showings, false))}}
+                </div>
+                <div class="movie-dates" v-else>
+                    Le {{dateToString(findExtremitySceance(movie.showings, true))}}
                 </div>
             </div>
         </div>
+        
     </BreezeAuthenticatedLayout>
 </template>
 
@@ -33,7 +42,79 @@ import { Head } from '@inertiajs/inertia-vue3';
 export default defineComponent({
     props:{
         movies: Array,
+    },
+
+    mounted(){
+    },
+
+    methods:{
+        findExtremitySceance(sceances, first=false){
+            let current = null;
+            for(let sceance of sceances){
+                if(!current && sceance.begin){
+                    current = sceance.begin
+                }
+                else{
+                    if(first && Date.parse(sceance.begin) <= Date.parse(current)){
+                        current = sceance.begin
+                    } else if(!first && Date.parse(sceance.begin) >= Date.parse(current)){
+                        current = sceance.begin
+                    }
+                }
+            }
+            return Date.parse(current)
+        },
+
+        dateToString(date){
+            return new Date(date).toLocaleDateString();
+        }
     }
 })
 
 </script>
+
+<style scoped>
+.movie-rectangle {
+    width: 215px;
+    background: #22577A;
+    color: #F4F3F7;
+    margin: 10px 10px 10px 10px;
+    cursor: pointer;
+}
+
+.movie-rectangle:hover {
+    filter:brightness(120%);
+}
+
+.movie-poster img{
+    width: 215px;
+    height: 323px;
+}
+
+.movie-title {
+    font-weight: bold;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.movie-title span {
+    font-family: 'Nunito-black';
+    text-align: center;
+}
+
+.movie-dates {
+    height: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.movie-wrapper {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+</style>
