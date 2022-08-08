c4e1d4120b6444dd0f13dfc2d8bdf62d13e2fac4
Florent Marcel <florent.marcel@keysource.be>
2022-08-08 17:23:06 +0200

    User filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'firstname',
+        'lastname',
+        'is_blocked',
+        'is_admin',
     ];
 
     /**
