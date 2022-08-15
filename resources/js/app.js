3582fe68b5239e9242bf1dacf9d7ad3b6fea09a8
Florent Marcel <Florent.Marcel@hotmail.com>
2022-08-15 22:16:57 +0200

    global function $t to use the translated field

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,6 +15,7 @@ import moment from 'moment';
 import 'moment/dist/locale/fr';
 import 'moment/dist/locale/en-gb';
 import globalMixin from './base'
+import { usePage } from '@inertiajs/inertia-vue3'
 
 library.add(faUser, faCouch, faCircleXmark, faFilePdf)
 
@@ -63,6 +64,11 @@ createInertiaApp({
             let nbMinutes = Math.floor(seconds /60);
             let nbSeconds = seconds % 60;
             return `${nbMinutes}m${nbSeconds}s`;
+        },
+
+        myApp.config.globalProperties.$t = (key) => {
+            let locale = usePage().props.value.locale
+            return `${key}_${locale}`;
         },
 
         myApp.config.globalProperties.$route = route
